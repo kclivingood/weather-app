59fd7d70e6c9ce732659eff1ce53df864d83f33e
kclivingood <42717009+kclivingood@users.noreply.github.com>
2020-02-21 19:00:56 -0500

    Reorganizing to be cleaner

--- public/js/app.js
+++ public/js/app.js
@@ -19,7 +19,7 @@ weatherForm.addEventListener('submit', (e) => {
     message.classList.remove('hide')
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-    
+
     fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data = {}) => {
             if (data.error) {
@@ -40,10 +40,8 @@ search.addEventListener('input', (e) => {
 
     message.classList.add('hide')
 
-    
-
     if (!location) {
-        document.querySelector('#auto-dropdown').classList.add('hide')
+        dropdownEl.classList.add('hide')
     } else {
         fetch(`/weather?address=${location}`).then((response) => {
             response.json().then((data = {}) => {
@@ -51,21 +49,23 @@ search.addEventListener('input', (e) => {
                     console.log(data.error)
                 } else {
                     for (let count = 0; count < 5; count++) {
+
                         const dropdownChild = dropdownEl.children[count]
+
                         dropdownChild.textContent = data.features[count].place_name
+
                         dropdownChild.addEventListener('click', (e) => {
                             search.value = data.features[count].place_name
-                            
-                            document.querySelector('#auto-dropdown').classList.add('hide')
+
+                            dropdownEl.classList.add('hide')
                             messageOne.textContent = ''
                             messageTwo.textContent = ''
                         })
                     }
 
-                    document.querySelector('#auto-dropdown').classList.remove('hide')
-                }
+                    dropdownEl.classList.remove('hide')
             })
         })
-        
+
     }
 })
